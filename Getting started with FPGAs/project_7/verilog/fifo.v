e07da54f6e216173fdd51ccb8b1521f47bdb1513
Zach Moolman <jzmmoolman@scapticom.co.za>
2024-11-10 22:11:43 -0700

    Project 7 - New FIFO module

---
+++ Getting started with FPGAs/project_7/verilog/fifo.v
@@ -0,0 +1,116 @@
+module FIFO #(
+    parameter WIDTH = 8,
+    DEPTH = 256
+) (
+    input                     i_rst_l,
+    input                     i_clk,
+    // write side
+    input                     i_wr_dv,
+    input  [       WIDHT-1:0] i_wr_data,
+    input  [clog2(DEPTH-1):0] i_af_level,
+    output                    o_af_flag,
+    output                    o_full,
+    // read side
+    input                     i_rd_en,
+    output                    o_rd_dv,
+    output [       WIDTH-1:0] o_rd_data,
+    input  [clog2(DEPTH-1):0] i_ae_level,
+    output                    o_ae_flag
+);
+
+  reg  [clog2(DEPTH-1):0] r_wr_addr;
+  reg  [clog2(DEPTH-1):0] r_rd_addr;
+  reg  [  clog2(DEPTH):0] r_count;
+
+  wire                    w_rd_dv;
+  wire [       WIDHT-1:0] w_rd_data;
+
+  RAM_2Port #(
+      .WIDHT(WIDTH),
+      .DEPTH(DEPTH)
+  ) ram_inst (
+      // write port
+      .i_wr_clk (i_clk),
+      .i_wr_data(r_wr_addr),
+      .i_wr_dv  (i_wr_dv),
+      .i_wr_data(i_wr_data),
+      // read prot
+      .i_rd_clk (i_clk),
+      .i_rd_addr(r_rd_addr),
+      .i_rd_en  (i_rd_en),
+      .o_rd_dv  (w_rd_dv),
+      .o_rd_data(w_rd_data)
+  );
+
+  always @(posedge i_clk or negedge i_rst_l) begin
+    if (~i_rst_l) begin
+      r_wr_addr <= 0;
+      r_rd_addr <= 0;
+      r_count   <= 0;
+    end else begin
+      // write
+      if (i_wr_dv) begin
+        if (r_wr_addr == DEPTH - 1) begin
+          r_wr_addr <= 0;
+        end else begin
+          r_wr_addr <= r_wr_addr + 1;
+        end
+      end
+
+      // read
+      if (i_rd_dv) begin
+        if (r_rd_addr == DEPTH - 1) begin
+          r_rd_addr <= 0;
+        end else begin
+          r_rd_addr <= r_rd_addr + 1;
+        end
+      end
+
+      // read with no writes
+      if (i_rd_en & ~i_wr_dv) begin
+        if (r_count != 0) begin
+          r_count <= r_count - 1;
+        end
+        // write with no rea
+      end else if (i_wr_dv & ~i_rd_en) begin
+        if (r_count != DEPTH) begin
+          r_count <= r_count + 1;
+        end
+      end
+
+      if (i_rd_en) begin
+        o_rd_data <= w_rd_data;
+      end
+
+    end
+  end
+
+
+  assign o_full = (r_count == DEPTH) || (r_count == DEPTH - 1 && i_wr_dv && !i_rd_en);
+  assign o_empty = (r_count == 0);
+
+  assign o_AF_Flag = (r_Count > DEPTH - i_AF_Level);
+  assign o_AE_Flag = (r_Count < i_AE_Level);
+
+  assign o_rd_en = w_rd_dv;
+
+  /////////////////////////////////////////////////////////////////////////////
+  // ASSERTION CODE, NOT SYNTHESIZED
+  // synthesis translate_off
+  // Ensures that we never read from empty FIFO or write to full FIFO.
+  always @(posedge i_Clk) begin
+    if (i_Rd_En && !i_Wr_DV && r_Count == 0) begin
+      $error("Error! Reading Empty FIFO");
+    end
+
+    if (i_Wr_DV && !i_Rd_En && r_Count == DEPTH) begin
+      $error("Error! Writing Full FIFO");
+    end
+  end
+  // synthesis translate_on
+  /////////////////////////////////////////////////////////////////////////////
+
+
+
+endmodule
+
---
+++ Getting started with FPGAs/project_7/verilog/fifo_tb.v
@@ -0,0 +1,148 @@
+// Russell Merrick - http://www.nandland.com
+//
+// FIFO testbench.
+
+module FIFO_TB ();
+
+  localparam DEPTH = 4;
+  localparam WIDTH = 8;
+
+  reg r_clk = 1'b0, r_rst_L = 1'b0;
+  reg r_wr_dv = 1'b0, r_rd_en = 1'b0;
+  reg [WIDTH-1:0] r_wr_data = 0;
+  reg [$clog2(DEPTH)-1:0] r_af_level = DEPTH-1, r_ae_level = 1;
+  wire w_af_flag, w_ae_flag, w_full, w_empty, w_rd_dv;
+  wire [WIDTH-1:0] w_rd_data;
+  
+  FIFO #(.WIDTH(WIDTH), .DEPTH(DEPTH)) UUT 
+  (
+  .i_rst_l(r_rst_l),
+  .i_clk(r_clk),
+  // write side
+  .i_wr_dv(r_wr_dv),
+  .i_wr_data(r_wr_data),
+  .i_af_level(r_af_level),
+  .o_af_flag(w_af_flag),
+  .o_full(w_full),
+  // read side
+  .i_rd_en(r_rd_en),
+  .o_rd_dv(w_rd_dv),
+  .o_rd_data(w_rd_data),
+  .i_ae_level(r_ae_level),
+  .o_ae_flag(w_ae_flag),
+  .o_empty(w_empty)
+  );
+
+  always #10 r_clk <= !r_clk; // create oscillating clock
+
+  // this task triggers a reset condition to the fifo.
+  task reset_fifo();
+    @(posedge r_clk);
+    r_rst_l <= 1'b0;
+    r_wr_dv <= 1'b0;  // ensure rd/wr are off
+    r_rd_en <= 1'b0;
+    @(posedge r_clk);
+    r_rst_l <= 1'b1;
+    @(posedge r_clk);
+    @(posedge r_clk);
+  endtask
+
+
+  initial
+  begin 
+    integer i;
+
+  reset_fifo();
+
+    // Write single word, ensure it appears on output
+    r_wr_dv   <= 1'b1;
+    r_wr_data <= 8'hAB;
+    @(posedge r_clk);
+    r_wr_dv   <= 1'b0;
+    @(posedge r_clk);
+    assert (!w_empty);
+
+    repeat(4) @(posedge r_clk);
+
+    // read out that word, ensure dv and empty is correct
+    r_rd_en <= 1'b1;
+    @(posedge r_clk);
+    r_rd_en <= 1'b0;
+    @(posedge r_clk);
+    assert (w_rd_dv);
+    assert (w_empty);
+    assert (w_rd_data == 8'hAB);
+
+
+    // Test: Fill FIFO with incrementing pattern, then read it back.
+    reset_fifo();
+    r_Wr_Data <= 8'h30;
+    repeat(DEPTH)
+    begin
+      r_wr_dv <= 1'b1;
+      @(posedge r_clk);
+      r_wr_dv <= 1'b0;
+      @(posedge r_clk);
+      r_wr_data <= r_wr_data + 1;
+    end
+    r_wr_dv <= 1'b0;
+    @(posedge r_clk);
+    assert (w_full);
+    @(posedge r_clk);
+
+    // Read out and verify incrementing pattern
+    for (i=8'h30; i<8'h30 + DEPTH; i=i+1)
+    begin
+      r_rd_en <= 1'b1;
+      @(posedge r_clk);
+      r_rd_en <= 1'b0;
+      @(posedge r_clk);
+      assert (w_rd_dv);
+      assert (w_rd_data == i) else $error("rd_data is %d i is %d", w_rd_data, i);
+      @(posedge r_clk);
+    end
+    assert (w_empty);
+
+
+    // Test: Read and write on same clock cycle when empty + full
+    reset_fifo();
+    r_rd_en <= 1'b1;
+    r_wr_dv <= 1'b1;
+    @(posedge r_clk);
+    @(posedge r_clk);
+    r_rd_en <= 1'b0;
+    repeat(DEPTH) @(posedge r_clk);
+    assert (w_full);
+    r_rd_en <= 1'b1;
+    @(posedge r_clk);
+    assert (w_full);
+    @(posedge r_clk);
+    assert (w_full);
+    r_wr_dv <= 1'b0;
+    r_rd_en <= 1'b0;
+
+    // Test: Almost Empty, Almost Full Flags
+    // AE is set to 1, AF is set to 3
+    reset_fifo();
+    assert (w_ae_flag);
+    assert (!w_af_flag);
+    
+    r_wr_dv <= 1'b1;
+    @(posedge r_clk);
+    assert (w_ae_flag);
+    assert (!w_af_flag);
+    @(posedge r_clk);
+    assert (!w_ae_flag);
+    assert (!w_af_flag);
+    @(posedge r_clk);
+    assert (!w_ae_flag);
+    assert (w_af_flag);
+    @(posedge r_clk);
+    assert (!w_ae_flag);
+    assert (w_af_flag);
+    assert (w_full);  
+
+    $finish();
+  end
+
+endmodule
